e792edbae58d1f0e3498a40a502211d449ede7d1
Cameron Huysmans <cameron.huysmans@gmail.com>
2021-05-07 09:20:47 +1000

    updated to TF 0.12

--- main.tf
+++ main.tf
@@ -1,22 +1,27 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
-  profile = "${var.aws_profile}"
+  region = var.aws_region
+  profile = var.aws_profile
 }
 
 resource "aws_instance" "ubuntu" {
-  ami               = "${var.ami_id}"
-  instance_type     = "${var.instance_type}"
+  ami               = var.ami_id
+  instance_type     = var.instance_type
   availability_zone = "${var.aws_region}a"
 
 
   tags {
-    Name        = "${var.name}"
-    TTL         = "${var.ttl}"
-    Owner       = "${var.owner}"
+    Name        = var.name
+    TTL         = var.ttl
+    Owner       = var.owner
     Description = "This is a demo qa description"
     Customer    = "HashiCorp-Demo-aws"
   }
