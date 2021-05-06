ffb48a9d9a2dfe536d3c156d8220b75721a2f518
Cameron Huysmans <cameron.huysmans@gmail.com>
2021-05-07 09:53:49 +1000

    added the vpc

--- main.tf
+++ main.tf
@@ -30,3 +30,29 @@ resource "aws_instance" "ubuntu" {
 output "instance_ip_address" {
   value = aws_instance.ubuntu.private_ip
 }
+
+module "vpvc" {
+  source  = "app.terraform.io/camhuysmans/vpvc/aws"
+  version = "3.0.0"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Name        = var.name
+    TTL         = var.ttl
+    Owner       = var.owner
+    Description = "This is a demo qa description"
+    Customer    = "HashiCorp-Demo-aws"
+    Environment = "qa"
+  }
+}
+output "vpc_id" {
+  value = module.vpvc.vpc_id
+}
