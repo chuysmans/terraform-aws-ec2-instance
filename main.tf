18928fa9f40943dd960a7357a9b1dd2bdb843d66
Cameron Huysmans <cameron.huysmans@gmail.com>
2021-05-07 11:07:50 +1000

    added this VPC again

--- main.tf
+++ main.tf
@@ -31,28 +31,28 @@ output "instance_ip_address" {
   value = aws_instance.ubuntu.private_ip
 }
 
-// module "vpvc" {
-//   source  = "app.terraform.io/camhuysmans/vpvc/aws"
-//   version = "3.0.0"
-//   name = "my-vpc"
-//   cidr = "10.0.0.0/16"
+module "vpvc" {
+  source  = "app.terraform.io/camhuysmans/vpvc/aws"
+  version = "3.0.0"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-//   azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-//   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-//   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-//   enable_nat_gateway = true
-//   enable_vpn_gateway = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-//   tags = {
-//     Name        = var.name
-//     TTL         = var.ttl
-//     Owner       = var.owner
-//     Description = "This is a demo qa description"
-//     Customer    = "HashiCorp-Demo-aws"
-//     Environment = "qa"
-//   }
-// }
-// output "vpc_id" {
-//   value = module.vpvc.vpc_id
-// }
+  tags = {
+    Name        = var.name
+    TTL         = var.ttl
+    Owner       = var.owner
+    Description = "This is a demo qa description"
+    Customer    = "HashiCorp-Demo-aws"
+    Environment = "qa"
+  }
+}
+output "vpc_id" {
+  value = module.vpvc.vpc_id
+}
